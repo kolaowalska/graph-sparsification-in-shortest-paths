bfdfe960f231dccdc4abb9d348d9b1c5b7307f38
kolaowalska <normalnie.kowalska@student.uj.edu.pl>
2025-06-01 10:38:57 +0200

    tried adding plots with different rho values

--- src/graph_pipeline/__init__.py
+++ src/graph_pipeline/__init__.py
@@ -6,12 +6,24 @@ from utils.visualizer import visualize_sparsification
 from sparsifiers import sparsifiers_registry
 from experiments import run_experiments as rexp
 from experiments.visualization import generate_all_plots as plot
+from experiments.visualization.plots.plot_stretch_vs_rho import plot_stretch_vs_rho
 
 UNPROCESSED_DIR = Path("small_data/unprocessed")
 PROCESSED_DIR = Path("small_data/processed")
 RESULTS_DIR = Path("./small_results")
 IMAGES_DIR = RESULTS_DIR / "images"
+
+MULTI_RHO = False
+# RHO = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0] if MULTI_RHO else [0.2]
 RHO = 0.2
+
+## TODO:
+'''
+- dodac wykresy pokazujace rozbieznosc roznych metryk w zaleznosci od prune rate 
+dla kazdego sparsyfikatora w obrebie 1 rodziny grafow?? za duzo slow to jest
+- przepuscic wieksze grafy przez ten kolchoz 
+- cos jeszcze wymysle
+'''
 
 
 def draw_small_graphs(family: str):
@@ -67,3 +79,36 @@ if __name__ == "__main__":
         rexp.main(rho=RHO, data_dir=data_dir, out_file=out_file, family=family)
         plot.generate_plots(out_file, RESULTS_DIR / f"{family}_plots")
         draw_small_graphs(family)
+
+
+    '''
+        for rho in RHO_VALUES:
+        suffix = f"rho_{str(rho).replace('.', '_')}" if MULTI_RHO else "main"
+        rho_dir = RESULTS_DIR / suffix
+        rho_dir.mkdir(parents=True, exist_ok=True)
+
+        for family in families:
+            print(f"\nRunning experiments for '{family}' at rho = {rho}")
+
+            data_dir = PROCESSED_DIR / family
+            if not data_dir.exists():
+                tqdm.write(f"Skipping missing family: {family}")
+                continue
+
+            out_file = rho_dir / f"{family}_results.csv"
+
+            setattr(rexp, 'data_dir', data_dir)
+            setattr(rexp, 'out_file', out_file)
+            setattr(rexp, 'family', family)
+
+            rexp.main(rho=rho, data_dir=data_dir, out_file=out_file, family=family)
+
+            # plot.generate_plots(out_file, rho_dir / f"{family}_plots")
+
+            if MULTI_RHO:
+                plot_stretch_vs_rho(
+                    base_results_dir=RESULTS_DIR,
+                    out_path=RESULTS_DIR / "collective_plots" / "stretch_vs_rho.png"
+                )
+    '''
+
